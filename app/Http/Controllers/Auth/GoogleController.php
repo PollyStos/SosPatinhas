d536653cfbd05571a19c9d5c65ff3cc6e5fec736
polly_stos <polyana.santos@gigapixel.com.br>
2025-04-14 07:54:59 -0300

    primeiro commit - sistema de login e autenticação concluido

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'email_verified_at' => now(),
+                'password' => Hash::make(Str::random(16)),
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect()->intended('/dashboard');
+    }
+}

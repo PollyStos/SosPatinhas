105406e900c550c28919c09f840092ca5018b3ee
PollyStos <polly_stos@yahoo.com.br>
2025-04-22 09:08:17 -0300

    criação de componentes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.layout')
-@section('title','Home')
-@section('content')
 
+@section('title','Home')
+
+@section('content')
+    @include('component.about')
+    @include('component.galery')
+    @include('component.galery')
+    @include('component.feature')
+    @include('component.depoiment')
+    @include('component.blog')
+    @include('component.message')
 @endsection

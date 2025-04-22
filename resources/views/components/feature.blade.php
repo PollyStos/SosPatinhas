dc1f85231cd6fda29f539e47e4b27627d1ccd956
WAJoao <163187177+WAJoao@users.noreply.github.com>
2025-04-22 18:15:24 -0300

    Update feature.blade.php

---
+++ resources/views/components/feature.blade.php
@@ -0,0 +1,11 @@
+<section class="py-16 text-center" style="background-color: #058158;">
+    <p class="max-w-3xl mx-auto text-base md:text-lg mb-8" style="color: #f0f8ff;">
+        Apoie SOS Patinhas
+    </p>
+
+    <button
+        class="px-8 py-3 rounded-full font-semibold" 
+        style="background-color: #f0f8ff; color: #000;">
+        Apoie
+    </button>
+</section>

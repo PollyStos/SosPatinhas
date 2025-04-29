935ce43564f055026cf82e4bb260a01f41b5043b
WAJoao <163187177+WAJoao@users.noreply.github.com>
2025-04-29 20:35:42 -0300

    Update about.blade.php

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -4,11 +4,7 @@
             Sobre a SOS Patinhas
         </h2>
         <p class="text-justify text-sm leading-relaxed text-gray-800">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-            <br><br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-            <br><br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
+            No SOS Patinhas, nossa missão vai muito além de apenas encontrar pets perdidos. Queremos reunir famílias, restaurar lares e devolver a alegria que só um animal de estimação pode trazer.
         </p>
     </div>
 </section>
